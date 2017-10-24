26bdb7391e0af317ae505e60d87eb3810e1335b1
Unknown <Brockmessick28@gmail.com>
2017-10-23 20:09:30 -0600

    chicken AI

---
+++ Chicken game/Assets/Scrypts/WolfAI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfAI : MonoBehaviour {
+
+	// Use this for initialization
+	
+
+public Rigidbody enemy; 
+
+public float moveSpeed; 
+
+public Transform target; 
+
+void OnTriggerStay(Collider other)
+{
+
+	if(other.gameObject.name == "Player"){
+		transform.LookAt(target);
+		transform.Translate(Vector3.forward*+moveSpeed*Time.deltaTime);
+
+	}
+	
+}
+
+		
+	
+	
+	// Update is called once per frame
+	
+}
